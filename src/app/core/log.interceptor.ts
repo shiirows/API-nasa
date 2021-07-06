0933bbb8e4d108a79c22da9eb407ff91e80aabef
Alexandre Mazoyer <mazoyeralexandre@hotmail.fr>
2021-07-06 09:35:44 +0200

    correction

--- src/app/core/log.interceptor.ts
+++ src/app/core/log.interceptor.ts
@@ -6,20 +6,19 @@ import { tap } from 'rxjs/operators';
 @Injectable()
 export  class  LogInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log(req.urlWithParams); // J'affiche l'url avec les params
-                // Je passe la requête à la suite des interceptors cachés d'angular
-               
-                // Et je retourne ce résultat pour que la requête ait bien lieu
-                   // pipe permet d'ajouter des actions à effectuer quand l'observable est résolue
+        console.log(req.urlWithParams); 
+
+        var start = new Date().getTime();
         return  next.handle(req).pipe(
             // tap est un observable qui indique: 
-            
+             
             // Je fais des actions sans modifier la réponse
             tap((response) => {
+                   
                     // On s'assure qu'il s'agit bien d'une réponse http
                     if (response  instanceof  HttpResponse) {
                             // Ici sera le code exécuté à la réponse du serveur
-                            var start = new Date().getTime();
+                            
                             var elapsed = new Date().getTime() - start ;
 
                           console.log(elapsed);
